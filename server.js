78f20871578015ca0a659bafa987b8d04cbb2370
Maria Kasińska <mariakasinska17@gmail.com>
2020-03-10 21:33:00 +0100

    add websocket evets listeners

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const socket = require('socket.io');
 
 const app = express();
 
+const tasks = [];
+
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/client')));
 
@@ -14,4 +16,19 @@ app.get('/', (req, res) => {
 
 const server = app.listen(8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', socket => {
+  socket.on('updateData', () => {
+    io.to(socket.id).emit('updateData', tasks);
+  });
+  socket.on('addTask', task => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+  socket.on('removeTask', index => {
+    tasks.splice(index, 1);
+  });
 });
