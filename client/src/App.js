eae2ed49fa9f291cc79517d86fab35f012e7afb5
Maria Kasińska <mariakasinska17@gmail.com>
2021-10-04 22:53:32 +0200

    add edit task options wo websockets

--- client/src/App.js
+++ client/src/App.js
@@ -7,24 +7,25 @@ class App extends React.Component {
     super(props);
     this.state = {
       tasks: [],
-      taskName: ''
+      taskName: '',
+      editedTask: {},
     };
   }
   componentDidMount() {
     this.socket = io('localhost:8000');
-    this.socket.on('updateData', tasks => {
+    this.socket.on('updateData', (tasks) => {
       this.updateTasks(tasks);
     });
-    this.socket.on('addTask', task => {
+    this.socket.on('addTask', (task) => {
       this.addTask(task);
     });
-    this.socket.on('removeTask', id => {
+    this.socket.on('removeTask', (id) => {
       this.removeTask(id);
     });
   }
   removeTask(id, isLocal) {
     this.setState({
-      tasks: this.state.tasks.filter(task => task.id !== id)
+      tasks: this.state.tasks.filter((task) => task.id !== id),
     });
     if (isLocal) {
       this.socket.emit('removeTask', id);
@@ -38,6 +39,7 @@ class App extends React.Component {
     const newTask = { name: this.state.taskName, id: uuidv4() };
     this.addTask(newTask);
     this.socket.emit('addTask', newTask);
+    this.setState({ taskName: '' });
   }
   addTask(newTask) {
     this.setState({ tasks: [...this.state.tasks, newTask] });
@@ -45,8 +47,28 @@ class App extends React.Component {
   updateTasks(taskslist) {
     this.setState({ tasks: taskslist });
   }
+  setEdit(task) {
+    this.setState({ editedTask: task });
+  }
+  editTask(value) {
+    this.setState({ editedTask: { ...this.state.editedTask, name: value } });
+  }
+  submitEdit(e) {
+    e.preventDefault();
+    //change state.tasks
+    this.setState({
+      tasks: this.state.tasks.map((task) =>
+        task.id === this.state.editedTask.id
+          ? { ...task, name: this.state.editedTask.name }
+          : task
+      ),
+    });
+    //emit change
+    //clear editedTask
+    this.setState({ editedTask: {} });
+  }
   render() {
-    const { tasks } = this.state;
+    const { tasks, editedTask } = this.state;
     return (
       <div className='App'>
         <header>
@@ -55,21 +77,46 @@ class App extends React.Component {
 
         <section className='tasks-section' id='tasks-section'>
           <h2>Tasks</h2>
-
           <ul className='tasks-section__list' id='tasks-list'>
-            {tasks.map(task => (
+            {tasks.map((task) => (
               <li className='task' key={task.id}>
                 {task.name}
-                <button
-                  className='btn btn--red'
-                  onClick={e => this.removeTask(task.id, true)}
-                >
-                  Remove
-                </button>
+                <div className='buttons'>
+                  <button
+                    className='btn'
+                    onClick={(e) => this.setEdit(task, true)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className='btn btn--red'
+                    onClick={(e) => this.removeTask(task.id, true)}
+                  >
+                    Remove
+                  </button>
+                </div>
               </li>
             ))}
           </ul>
-
+          {Object.keys(editedTask).length !== 0 && (
+            <form id='edit-task-form'>
+              <input
+                className='text-input'
+                autoComplete='off'
+                type='text'
+                value={this.state.editedTask.name}
+                id='task-name'
+                onChange={(e) => this.editTask(e.target.value)}
+              />
+              <button
+                className='btn'
+                type='submit'
+                onClick={(e) => this.submitEdit(e)}
+              >
+                Change
+              </button>
+            </form>
+          )}
           <form id='add-task-form'>
             <input
               className='text-input'
@@ -77,12 +124,12 @@ class App extends React.Component {
               type='text'
               placeholder='Type your description'
               id='task-name'
-              onChange={e => this.updateTask(e.target.value)}
+              onChange={(e) => this.updateTask(e.target.value)}
             />
             <button
               className='btn'
               type='submit'
-              onClick={e => this.submitForm(e)}
+              onClick={(e) => this.submitForm(e)}
             >
               Add
             </button>
--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const tasks = [];
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/client')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/client/index.html'));
+// });
 
 const server = app.listen(8000, () => {
   console.log('Server is running on port: 8000');
@@ -20,16 +20,15 @@ const server = app.listen(8000, () => {
 
 const io = socket(server);
 
-io.on('connection', socket => {
-  socket.on('updateData', () => {
-    io.to(socket.id).emit('updateData', tasks);
-  });
-  socket.on('addTask', task => {
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+  socket.on('addTask', (task) => {
     tasks.push(task);
     socket.broadcast.emit('addTask', task);
+    console.log(tasks);
   });
-  socket.on('removeTask', id => {
-    tasks.filter(task => task.id !== id);
+  socket.on('removeTask', (id) => {
+    tasks.filter((task) => task.id !== id);
     socket.broadcast.emit('removeTask', id);
   });
 });
